abc6a8a2eeeb2126c317d691e6a533bde2606290
Nick Lindeberg <lindebergnick@gmail.com>
2018-07-15 13:13:57 -0600

    updates player move method and test, not working currently

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,13 @@
 class Player
 
+
+  def player_move(move)
+    if move.upcase == ("A" || "B" || "C" || "D" || "E" || "F" || "G")
+
+
+
+    end
+  end
+
+
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -5,12 +5,16 @@ require "./lib/player.rb"
 class PlayerTest <Minitest::Test
 
   def test_runner_exists
-    cf = Player.new
+    play = Player.new
 
-    assert_instance_of Player, cf
+    assert_instance_of Player, play
   end
 
+  def test_if_player_can_choose_column
+    play = Player.new
 
+    assert_equal "Thanks for your guess", play.player_move("a")
+  end
 
 
 end
