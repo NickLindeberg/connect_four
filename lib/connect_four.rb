29c1a1642fa13ef8dccc101921613089cde11f10
Nick Lindeberg <lindebergnick@gmail.com>
2018-07-17 19:38:52 -0600

    updates choose_column method

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -8,4 +8,5 @@ player = Player.new
 puts "\u2015 Welcome to Connect Four! Please choose a letter to start the game! \u2015"
 # puts board.print_board
 # gets = input
-puts player.player_move("B")
+puts player.choose_column("c")
+puts player.choose_column("c")
--- lib/player.rb
+++ lib/player.rb
@@ -4,7 +4,7 @@ class Player
   attr_reader :player_move,
               :piece_drop,
               :gameboard,
-              :choose_row
+              :choose_column
 
 
   def initialize
@@ -12,26 +12,26 @@ class Player
     @turn = 0
   end
 
-  def choose_row(letter)
-    number = 6
+
+  if turn is even?
+    def piece_type
+      if @turn == 0
+        "X"
+      else
+        "O"
+      end
+    end
+
+  def choose_column(letter)
+    number = (6)
     chosen_column = player_move(letter)
-    index_assigned = access_index(chosen_column)
-    piece_drop(number, index_assigned)
+    row_column_chosen = piece_drop(6, chosen_column)
   end
-
-#if turn is even?
-  # def piece_type
-  #   if @turn == 0
-  #     "X"
-  #   else
-  #     "O"
-  #   end
-  # end
-
 
   def player_move(move)
     if ("A".."G").include?(move.upcase)
       "\nThank You!"
+      access_index(move.upcase)
     else
       "\nPlease choose one of the following letters ABCDEFG"
     end
@@ -44,7 +44,7 @@ class Player
   def piece_drop(number, column)
     if @gameboard.board[6][column] == "."
       @gameboard.board[6].delete_at(column)
-      @gameboard.board[6].insert(column, piece_type)
+      @gameboard.board[6].insert(column, "X")
 
     elsif @gameboard.board[5][column] == "."
       @gameboard.board[5].delete_at(column)
